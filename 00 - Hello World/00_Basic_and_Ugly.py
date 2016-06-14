dd933b258fb7ed8dc6ab8d78c768fcdf4aab20c2
Bryson Tyrrell <bryson.tyrrell@jamfsoftware.com>
2016-06-14 15:04:47 -0500

    Hello world

---
+++ 00 - Hello World/00_Basic_and_Ugly.py
@@ -0,0 +1,23 @@
+import Tkinter as tk
+
+
+class App(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master.title("Hello World")
+
+        text = tk.Label(self, text="This is your first GUI. (highfive)")
+        text.pack()
+
+        self.pack()
+
+
+if __name__ == '__main__':
+    """The most basic Tkinter example.
+
+    There are a lot of ways around the web that show you how to init a basic Tkinter GUI. This combination of creating
+    the root Tkinter.Tk object from the main function and passing it into a class that sub-classes Tkinter.Frame is what
+    I have settled on personally. There are many ways to accomplish this for simple Tkinter GUIs."""
+    root = tk.Tk()
+    app = App(root)
+    app.mainloop()
